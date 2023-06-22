37b7af6191c9eff0c9b4516150f8fb49eafaeaaf
Ojas <ojasbendale18200@gmail.com>
2023-06-22 16:49:23 +0530

    updated

--- src/component/AdvanceTax.jsx
+++ src/component/AdvanceTax.jsx
@@ -205,19 +205,15 @@ function AdvanceTax() {
       setAssessedTax(assessedTax);
     }
 
-    if (incomeTax === 0) {
-    } else if (incomeTax > 0 && incomeTax < 10000) {
-    } else {
-      var juneTax = incomeTax * 0.15;
-      var septTax = incomeTax * 0.3;
-      var decTax = incomeTax * 0.3;
-      var marTax = incomeTax * 0.25;
+    var juneTax = incomeTax * 0.15;
+    var septTax = incomeTax * 0.3;
+    var decTax = incomeTax * 0.3;
+    var marTax = incomeTax * 0.25;
 
-      setJune(juneTax);
-      setSept(septTax);
-      setDec(decTax);
-      setMar(marTax);
-    }
+    setJune(juneTax);
+    setSept(septTax);
+    setDec(decTax);
+    setMar(marTax);
   };
 
   const handleIsSection115BACChange = (e) => {
@@ -477,40 +473,44 @@ function AdvanceTax() {
           </Button>
         </form>
       </Container>
-      <Box w={"80%"} m={"auto"} mt={"80px"}>
-        <Text fontSize={"20px"} fontWeight={"500"}>
-          Advance Tax liability
-        </Text>
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              <Th>Description</Th>
-              <Th>Advance Tax Liability</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td>Advance tax payable upto June 15, 2023 (Cumulative)</Td>
-              <Td>{june}</Td>
-            </Tr>
-            <Tr>
-              <Td>Advance tax payable upto September 15, 2022 (Cumulative)</Td>
+      {incomeTax > 10000 && (
+        <Box w={"80%"} m={"auto"} mt={"80px"}>
+          <Text fontSize={"20px"} fontWeight={"500"}>
+            Advance Tax liability
+          </Text>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Description</Th>
+                <Th>Advance Tax Liability</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Advance tax payable upto June 15, 2023 (Cumulative)</Td>
+                <Td>{june}</Td>
+              </Tr>
+              <Tr>
+                <Td>
+                  Advance tax payable upto September 15, 2022 (Cumulative)
+                </Td>
 
-              <Td>{sept}</Td>
-            </Tr>
-            <Tr>
-              <Td>Advance tax payable upto December 15, 2022 (Cumulative)</Td>
+                <Td>{sept}</Td>
+              </Tr>
+              <Tr>
+                <Td>Advance tax payable upto December 15, 2022 (Cumulative)</Td>
 
-              <Td>{dec}</Td>
-            </Tr>
-            <Tr>
-              <Td>Advance tax payable upto March 15, 2023 (Cumulative)</Td>
+                <Td>{dec}</Td>
+              </Tr>
+              <Tr>
+                <Td>Advance tax payable upto March 15, 2023 (Cumulative)</Td>
 
-              <Td>{mar}</Td>
-            </Tr>
-          </Tbody>
-        </Table>
-      </Box>
+                <Td>{mar}</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      )}
     </Box>
   );
 }
